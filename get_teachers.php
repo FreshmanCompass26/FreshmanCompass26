b7ad4dda312ee767a173baedcb83d81653bce752
Milena-Esp <fatima.chavez2027@adoc.superate.org.sv>
2026-06-03 14:21:53 -0600

    cambiosbere

---
+++ get_teachers.php
@@ -0,0 +1,84 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+// conexión
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "teachers";
+
+$conn = new mysqli(
+    $servername,
+    $username,
+    $password,
+    $dbname
+);
+
+if($conn->connect_error){
+
+die(json_encode([
+"error"=>"Conexión fallida: ".$conn->connect_error
+]));
+
+}
+
+
+// consulta
+
+$sql = "SELECT
+id_teacher,
+nombre,
+materia,
+correo,
+dias,
+horario,
+frase,
+imagen,
+cumple
+FROM usuarios";
+
+
+$result = $conn->query($sql);
+
+$teachers=[];
+
+if($result && $result->num_rows>0){
+
+while($row=$result->fetch_assoc()){
+
+$teachers[]=[
+
+"id_teacher"=>$row["id_teacher"],
+
+"nombre"=>$row["nombre"],
+
+"materia"=>$row["materia"],
+
+"correo"=>$row["correo"],
+
+"dias"=>$row["dias"],
+
+"horario"=>$row["horario"],
+
+"frase"=>$row["frase"],
+
+"imagen"=>$row["imagen"],
+
+"cumple"=>$row["cumple"]
+
+];
+
+}
+
+}
+
+echo json_encode(
+$teachers,
+JSON_UNESCAPED_UNICODE
+);
+
+$conn->close();
+
+?>

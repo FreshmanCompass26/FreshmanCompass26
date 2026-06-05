8ea67737302426ce6c326f621ef6432f84ce6bdd
KarlaLuna301295 <karla.luna2027@adoc.superate.org.sv>
2026-06-04 19:26:39 -0600

    arreglos

--- php/conexion.php
+++ php/conexion.php
@@ -1,18 +1,14 @@
 <?php
 
-<<<<<<< HEAD
 // ===============================
 // CONEXIÓN A LA BASE DE DATOS
 // ===============================
 
-=======
->>>>>>> 00aaff8d652fe4528c2f5a710b39db98de9049ef
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "freshman_db";
 
-<<<<<<< HEAD
 // ===============================
 // CREAR CONEXIÓN
 // ===============================
@@ -23,20 +19,13 @@ $conn = new mysqli($host, $user, $pass, $db);
 // VERIFICAR CONEXIÓN
 // ===============================
 
-=======
-$conn = new mysqli($host, $user, $pass, $db);
-
->>>>>>> 00aaff8d652fe4528c2f5a710b39db98de9049ef
 if ($conn->connect_error) {
 
     die("Error de conexión: " . $conn->connect_error);
 
 }
 
-<<<<<<< HEAD
 // ⚠️ NO PONGAS ECHO AQUÍ
 // porque rompe los header(Location)
 
-=======
->>>>>>> 00aaff8d652fe4528c2f5a710b39db98de9049ef
 ?>

94b6272d81b193c80243ae4a375fc869953986f1
KarlaLuna301295 <karla.luna2027@adoc.superate.org.sv>
2026-06-17 21:12:20 -0600

    perfil terminado

--- php/actualizarprofile.php
+++ php/actualizarprofile.php
@@ -2,6 +2,8 @@
 
 include("profile_conexion.php");
 
+session_start(); // 🔥 AGREGA ESTO ARRIBA
+
 $idUsuario = 3;
 
 $nombre = $_POST['nombre'];
@@ -9,6 +11,18 @@ $fechaNacimiento = $_POST['fecha_nacimiento'];
 $email = $_POST['email'];
 $centroEscolar = $_POST['centro_escolar'];
 $username = $_POST['username'];
+$descripcion = $_POST['descripcion'];
+
+$fotoNombre = $_FILES['foto']['name'];
+
+if($fotoNombre != ""){
+    $rutaTemp = $_FILES['foto']['tmp_name'];
+    move_uploaded_file($rutaTemp, __DIR__ . "/../img/" . $fotoNombre);
+
+    $fotoSQL = ", foto_perfil='$fotoNombre'";
+} else {
+    $fotoSQL = "";
+}
 
 $sqlActualizar = "
 UPDATE estudiantes
@@ -17,12 +31,17 @@ nombre='$nombre',
 fecha_nacimiento='$fechaNacimiento',
 email='$email',
 centro_escolar='$centroEscolar',
-username='$username'
+username='$username',
+descripcion='$descripcion'
+$fotoSQL
 WHERE id=$idUsuario
 ";
 
 $conexion->query($sqlActualizar);
 
-header("Location: profile.php");
+// 🔥 ACTUALIZA LA SESIÓN AQUÍ
+$_SESSION['nombre'] = $nombre;
+
+header("Location: ../profile.php");
 exit();
 ?>

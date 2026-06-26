b46afb1172da231a7d2cf9d1cd6c60b76055fda3
ELDani-7 <daniel.rivas2027@adoc.superate.org>
2026-06-26 14:49:15 -0600

    Problemas en git, resolución

--- triviasuperate.php
+++ triviasuperate.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trivia ¡Súpérate! - FreshmanCompass</title>
-    <!-- Vinculamos tu archivo CSS separado -->
+    <title>Trivia ¡Súpérate!</title
     <link rel="stylesheet" href="styles/triviasp.css">
 </head>
 <body>

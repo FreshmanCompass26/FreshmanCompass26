ab5af03baf14c83247961d6fb694824499dbe686
Gene-s0 <genesis.serrano2027@adoc.superate.org.sv>
2026-06-27 20:36:09 -0600

    cambiosFooter

--- php/footer.php
+++ php/footer.php
@@ -1,40 +1,39 @@
 <footer class="footer">
+  <div class="footer-container">
 
-    <div class="footer-top">
-
-        <!-- Burbuja centrada -->
-        <div class="footer-bubble">
-            <h3>¿Necesitas ayuda?</h3>
-            <p>¡Estoy aquí para ayudarte!</p>
-        </div>
-
-        <!-- Logo derecha -->
-        <div class="footer-brand">
-            <img src="img/logoooooo.png" alt="Freshman Compass">
-            <p>Tu brújula para comenzar en ¡Supérate!</p>
-        </div>
-
+    <!-- Robot con burbuja -->
+    <div class="footer-robot">
+      <img src="img/robot.png" alt="Mascota Freshman Compass" class="robot-img">
+      <div class="footer-bubble">
+        <h3>¿Necesitas ayuda?</h3>
+        <p>¡Estoy aquí para ayudarte!</p>
+      </div>
     </div>
 
-    <div class="footer-divider"></div>
+    <!-- Logo y lema -->
+    <div class="footer-brand">
+      <img src="img/logo.png" alt="Freshman Compass" class="logo-img">
+      <p>Tu brújula para comenzar en ¡Supérate!</p>
+    </div>
 
+    <!-- Contactos -->
     <div class="footer-info">
-
-        <div class="info-item">
-            <i class="fab fa-instagram"></i>
-            <span>@freshmancompass</span>
-        </div>
-
-        <div class="info-item">
-            <i class="far fa-envelope"></i>
-            <span>info@freshmancompass.com</span>
-        </div>
-
-        <div class="info-item">
-            <i class="fas fa-school"></i>
-            <span>Centro ¡Supérate! ADOC</span>
-        </div>
-
+      <div class="info-item">
+        <i class="fa-brands fa-instagram"></i>
+        <p>@freshmancompass</p>
+      </div>
+      <div class="info-item">
+        <i class="fa-solid fa-envelope"></i>
+        <p>info@freshmancompass.com</p>
+      </div>
+      <div class="info-item">
+        <i class="fa-solid fa-house"></i>
+        <p>Centro ¡Supérate! ADOC</p>
+      </div>
     </div>
 
+  </div>
 </footer>
+
+
+

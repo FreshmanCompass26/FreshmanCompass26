4ae494d71930b2417ec54c89c0b2fe25afd69ff1
bere1011 <allison.alfaro2027@adoc.superate.org.sv>
2026-07-03 21:25:40 -0600

    eventos agreggados

--- mas-eventos.php
+++ mas-eventos.php
@@ -5,28 +5,25 @@ $pagina_actual = "eventos";
 
 include "php/conexion.php";
 
-$sql = "SELECT * FROM eventos ORDER BY evento_ID ASC LIMIT 4,5";
+// Mostrar todos los eventos a partir del quinto
+$sql = "SELECT * FROM eventos ORDER BY evento_ID ASC LIMIT 4,999";
 $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Más Eventos | Freshman Compass</title>
+    <title>Más Eventos | Freshman Compass</title>
 
-<link rel="stylesheet" href="styles/eventos.css">
-<link rel="stylesheet" href="styles/navbar.css">
+    <link rel="stylesheet" href="styles/eventos.css">
+    <link rel="stylesheet" href="styles/navbar.css">
 
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
-<link rel="stylesheet" href="styles/eventos.css">
-<link rel="stylesheet" href="styles/navbar.css">
-
-
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
@@ -35,7 +32,6 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
 <div class="main-content">
 
-    
     <div class="hero">
         <h1>Más eventos</h1>
 
@@ -47,77 +43,66 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
     <div class="events-grid">
 
-        <?php while($evento = $result->fetch_assoc()) { ?>
+        <?php while ($evento = $result->fetch_assoc()) { ?>
 
-        <div class="event-card">
+            <div class="event-card">
 
-            <img
-                src="img/<?php echo $evento['imagen']; ?>"
-                alt="<?php echo $evento['nombre']; ?>"
-                class="event-img"
-            >
+                <img
+                    src="img/<?php echo $evento['imagen']; ?>"
+                    alt="<?php echo $evento['nombre']; ?>"
+                    class="event-img">
 
-            <div class="event-content">
+                <div class="event-content">
 
-                <div class="event-date">
-                    <i class="fa-solid fa-calendar-days"></i>
+                    <div class="event-date">
+                        <i class="fa-solid fa-calendar-days"></i>
 
-                    <?php
-                    echo date(
-                        "d/m/Y",
-                        strtotime($evento['fecha'])
-                    );
-                    ?>
+                        <?php
+                        echo date(
+                            "d/m/Y",
+                            strtotime($evento['fecha'])
+                        );
+                        ?>
+                    </div>
+
+                    <h2>
+                        <?php echo $evento['nombre']; ?>
+                    </h2>
+
+                    <div class="event-location">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <?php echo $evento['ubicacion']; ?>
+                    </div>
+
+                    <p>
+                        <?php echo $evento['descripcion']; ?>
+                    </p>
+
+                    <a
+                        href="<?php echo $evento['enlace']; ?>"
+                        target="_blank"
+                        class="event-btn">
+                        Ver más
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </a>
+
                 </div>
-
-                <h2>
-                    <?php echo $evento['nombre']; ?>
-                </h2>
-
-                <div class="event-location">
-                    <i class="fa-solid fa-location-dot"></i>
-                    <?php echo $evento['ubicacion']; ?>
-                </div>
-
-                <p>
-                    <?php echo $evento['descripcion']; ?>
-                </p>
-
-                <a
-                    href="<?php echo $evento['enlace']; ?>"
-                    target="_blank"
-                    class="event-btn"
-                >
-                    Ver más
-                    <i class="fa-solid fa-arrow-right"></i>
-                </a>
 
             </div>
-
-        </div>
 
         <?php } ?>
 
     </div>
-    <div class="events-grid">
 
-    <?php while($evento = $result->fetch_assoc()) { ?>
+    <div class="more-container">
+        <a href="eventos.php" class="more-btn">
+            <i class="fa-solid fa-arrow-left"></i>
+            Volver a Eventos
+        </a>
+    </div>
 
-        <!-- card -->
-
-    <?php } ?>
-
-</div>
-
-<div class="more-container">
-    <a href="eventos.php" class="more-btn">
-        <i class="fa-solid fa-arrow-left"></i>
-        Volver a Eventos
-    </a>
-</div>
-
-</div>
 </div>
 
 </body>
+
 </html>

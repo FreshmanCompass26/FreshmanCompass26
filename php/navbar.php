3c4a118907e9ec7aea421c78ea5c8af106d114e1
bere1011 <allison.alfaro2027@adoc.superate.org.sv>
2026-07-04 12:02:49 -0600

    pequeños cambios

--- php/navbar.php
+++ php/navbar.php
@@ -23,7 +23,7 @@ if ($pagina_actual == "nuestro_centro") {
     <ul class="menu">
 
         <li class="<?= ($pagina_actual == 'inicio') ? 'active' : '' ?>">
-            <a href="index.php">
+            <a href="/freshmancompass26/index.php">
                 <i class="fa-solid fa-house"></i>
                 <span>Inicio</span>
             </a>

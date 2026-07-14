eeefd4a710d36c40574debfd92f97b2c45a8b3fe
KarlaLuna301295 <karla.luna2027@adoc.superate.org.sv>
2026-07-13 18:39:14 -0600

    media de consejos lista

--- consejos.php
+++ consejos.php
@@ -21,6 +21,7 @@ $resultado = $conn->query($sqlCards);
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consejos</title>
 
     <link rel="stylesheet" href="styles/consejos.css">
